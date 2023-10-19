072636a33aca659f1d8152180b218896f83d2956
Sumit Rajput <sr@g.com>
2023-10-19 17:13:07 +0530

    Added color

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,6 @@ import CssBaseline from "@mui/material/CssBaseline";
 import NavBar from "@/components/NavBar";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
-import styles from "@/styles/Home.module.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
@@ -15,9 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <NavBar></NavBar>
-        <main className={styles.unibg}>
-          <Component {...pageProps} />
-        </main>
+        <Component {...pageProps} />
       </ThemeProvider>
     </QueryClientProvider>
   );
